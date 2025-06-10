1528bd9696f21d88a2cd6db2e63147bc17755e2c
adrien gaultier <adrien.gaultier@protonmail.com>
2025-06-10 23:53:43 +0200

    add test

--- caracal-ebpf/src/deunhide/mod.rs
+++ caracal-ebpf/src/deunhide/mod.rs
@@ -1,4 +1,5 @@
 // based on https://www.unhide-forensics.info/
 // here the goal is to counter brute force methods
+// it's a wip, here we can test new methods on a hidden process with test.c
 pub mod stat;
 pub mod sys;
